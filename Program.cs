f78410f20b37aad599581e248d7b71f6fa172647
FaabsD <54630797+FaabsD@users.noreply.github.com>
2019-09-12 12:00:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace practicum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("###########################################################");
+            Console.WriteLine("##################                       ##################");
+            Console.WriteLine("################## Rock, Paper, Scissors ##################");
+            Console.WriteLine("##################                       ##################");
+            Console.WriteLine("###########################################################");
+
+            Console.ReadLine();
+        }
+    }
+}
